0ce764eb1a7b598854c3060dc7b40721f249d20f
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-02-07 12:43:43 +0530

    update

--- my-project/src/components/AddProduct.vue
+++ my-project/src/components/AddProduct.vue
@@ -1,26 +1,72 @@
 <template>
+    <form>
+      <div class="space-y-12">
+        <div class="border-b border-gray-900/10 pb-12 ml-5">
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-4">
+             <div class="mt-2">
+                
+                <div class="sm:col-span-3">
+              <label for="product-name" class="block text-sm/6 font-medium text-gray-900">Product Name</label>
+              <div class="mt-2">
+                <input type="text" name="product-name" id="product-name" autocomplete="off" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              </div>
+            </div>
+  
+            <div class="sm:col-span-3 mt-2">
+              <label for="price" class="block text-sm/6 font-medium text-gray-900">Price</label>
+              <div class="mt-2">
+                <input type="number" name="price" id="price" autocomplete="off" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              </div>
+            </div>
 
-<div>
-    <h1>Add Product</h1>
-    <div>
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name">
-    </div>
-    <div>
-        <label for="description">Description</label>
-        <input type="text" id="description" name="description">
-    </div>
-    <div>
-        <label for="price">Price</label>
-        <input type="text" id="price" name="price">
-    </div>
-    <div>
-        <label for="image">Image</label>
-        <input type="file" id="image" name="image">
-    </div>
-    <button type="submit">Submit</button>
-</div>
-</template>
-
-<script>
-</script>
+            <div class="sm:col-span-3 mt-2">
+              <label for="qty" class="block text-sm/6 font-medium text-gray-900">Quantity</label>
+              <div class="mt-2">
+                <input type="number" name="qty" id="qty" autocomplete="off" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              </div>
+            </div>
+              </div>
+            </div>
+  
+            <div class="col-span-full">
+              <label for="description" class="block text-sm/6 font-medium text-gray-900">Description</label>
+              <div class="mt-2">
+                <textarea name="description" id="description" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+              </div>
+              <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about the product.</p>
+            </div>
+  
+            
+  
+            <div class="col-span-full">
+              <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover Photo</label>
+              <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div class="text-center">
+                  <PhotoIcon class="mx-auto size-12 text-gray-300" aria-hidden="true" />
+                  <div class="mt-4 flex text-sm/6 text-gray-600">
+                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                      <span>Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                    </label>
+                    <p class="pl-1">or drag and drop</p>
+                  </div>
+                  <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      </div>
+    </form>
+  </template>
+  
+  <script setup>
+  import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
+  import { ChevronDownIcon } from '@heroicons/vue/16/solid'
+  </script>
--- my-project/src/components/Navbar.vue
+++ my-project/src/components/Navbar.vue
@@ -14,11 +14,11 @@
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            <img width="40" height="40" src="https://img.icons8.com/stitch/50/shop.png" alt="shop"/>
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-white hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-gray-500', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
             </div>
           </div>
         </div>
@@ -35,7 +35,7 @@
               <MenuButton class="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                 <span class="absolute -inset-1.5" />
                 <span class="sr-only">Open user menu</span>
-                <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                <img width="30" height="30" src="https://img.icons8.com/ultraviolet/40/user.png" alt="user"/>
               </MenuButton>
             </div>
             <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
@@ -58,7 +58,7 @@
 
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pt-2 pb-3">
-        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-white hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-gray-500', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
       </div>
     </DisclosurePanel>
   </Disclosure>
