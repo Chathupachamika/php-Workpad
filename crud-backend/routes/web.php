54dfb8752da2b073ed9617c73744182dc4d864d5
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-02-10 16:54:03 +0530

    update commit

--- crud-backend/routes/web.php
+++ crud-backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/users', [RegisteredUserController::class, 'index']);
+
+Route::apiResource('/posts', PostController::class);
